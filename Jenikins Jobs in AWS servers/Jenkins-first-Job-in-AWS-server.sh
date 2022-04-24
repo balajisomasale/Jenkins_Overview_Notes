bee7b9262bab58289728563fe44476bc2bd3904b
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2022-04-24 18:35:59 -0300

    Create Jenkins-first-Job-in-AWS-server.sh

---
+++ Jenikins Jobs in AWS servers/Jenkins-first-Job-in-AWS-server.sh
@@ -0,0 +1,9 @@
+# Executed in shell command
+# !/bin/bash
+echo "Jenkins job is running with the mentioned user below"
+whoami
+
+echo "The commands of this job are executed from below directory" 
+pwd
+
+touch test_jenkins_files{1..20}
