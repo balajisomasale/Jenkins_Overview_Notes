46cc217d722e2ca9dd488064d40c628fef9ec229
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2022-04-29 15:40:25 -0300

    Create Vprofile-Build-Job.sh

---
+++ Jenikins Jobs in AWS servers/Vprofile-Build-Job.sh
@@ -0,0 +1,5 @@
+Github : https://github.com/devopshydclub/vprofile-project.git
+branch to build : */vp-rem
+
+shell command : cp target/vprofile-v2.war target/vprofile-V$BUILD_ID-$BUILD_TIMESTAMP.war
+
